29fb90857dcfeb373512d06565e50cc3f1c998cc
juan <juancamilogaviriaacosta@gmail.com>
2017-04-05 12:10:25 -0500

    Color rojo: editar datos usuario

--- appbase/Functional_test.py
+++ appbase/Functional_test.py
@@ -84,3 +84,65 @@ class FunctionalTest(TestCase):
         label_usuario = self.browser.find_element_by_id('username')
 
         self.assertIn('juan645', label_usuario.text)
+
+    def test_5_editar(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_login')
+        link.click()
+        self.browser.implicitly_wait(10)
+
+        login_usuario = self.browser.find_element_by_id('id_username_login')
+        login_usuario.send_keys('juan645')
+
+        login_clave = self.browser.find_element_by_id('id_password_login')
+        login_clave.send_keys('clave123')
+
+        botonIngresar = self.browser.find_element_by_id('btn_login')
+        botonIngresar.click()
+
+        self.browser.find_element_by_id('id_editar').click()
+
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_register')
+        link.click()
+
+        nombre = self.browser.find_element_by_id('id_nombre')
+        nombre.send_keys('Juan Daniel')
+
+        apellidos = self.browser.find_element_by_id('id_apellidos')
+        apellidos.send_keys('Arevalo Editado')
+
+        experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
+        experiencia.send_keys('5')
+
+        self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
+        telefono = self.browser.find_element_by_id('id_telefono')
+        telefono.send_keys('3173024578')
+
+        correo = self.browser.find_element_by_id('id_correo')
+        correo.send_keys('jd.patino1@uniandes.edu.co')
+
+        imagen = self.browser.find_element_by_id('id_imagen')
+        imagen.send_keys('/home/juan/Escritorio/persona.jpg')
+
+        nombreUsuario = self.browser.find_element_by_id('id_username')
+        nombreUsuario.send_keys('juan645')
+
+        clave = self.browser.find_element_by_id('id_password')
+        clave.send_keys('clave123')
+
+        botonGrabar = self.browser.find_element_by_id('id_grabar')
+        botonGrabar.click()
+        self.browser.implicitly_wait(3)
+        span = self.browser.find_element(By.XPATH, '//span[text()="Juan Daniel Arevalo Editado"]')
+
+        self.assertIn('Juan Daniel Arevalo Editado', span.text)
+
+
+
+
+
+
+
+
+
